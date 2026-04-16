7b636abd94b8049bb8cf388e1a40e79b72fe3114
Sulakshani99 <139636218+Sulakshani99@users.noreply.github.com>
2026-04-16 18:28:41 +0530

    Created Eureka Discovery service to service-service communication

---
+++ discovery-service/src/test/java/com/ridebooking/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ridebooking.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
