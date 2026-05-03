e8d3ee3db9c9f8e8bb1234fdc51b288b105b0a2b
Sulakshani99 <139636218+Sulakshani99@users.noreply.github.com>
2026-05-04 00:20:06 +0530

    Revert "Created Notification service and implement to send emails to USER/DRIVER through AWS SES service when trigger Ride Event...."
    This reverts commit ec0f1d62

--- notification-service/src/main/java/com/ridebooking/notification/service/interfaces/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.ridebooking.notification.service.interfaces;
-
-public interface EmailService 
-{
-    void sendEmail(String to, String subject, String bodyHtml, String bodyText) throws Exception;
-}
